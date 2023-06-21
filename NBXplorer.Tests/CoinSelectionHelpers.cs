1fe547ccdadb66c45c44e2ed2a190795350d07d2
Rodrigo Sanchez <rodrigo@clovrlabs.com>
2023-06-21 09:39:09 +0200

    Removed tolerance, added comments, added Elenpay Dev to launchSettings

--- NBXplorer.Tests/CoinSelectionHelpers.cs
+++ NBXplorer.Tests/CoinSelectionHelpers.cs
@@ -48,26 +48,6 @@ public class CoinSelectionHelpersTests
 	}
 
 	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_Changeless_InsideToleranceBelow_OneUtxoCoversAll()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(8) },
-		};
-		int limit = 3;
-		long amount = 9;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(8) }, GetValues(result));
-	}
-
-
-	[Fact]
 	public void SelectCoins_ShouldReturnSelectedCoins_Changeless_InsideToleranceAbove_OneUtxoCoversAll()
 	{
 		// Arrange
@@ -77,10 +57,9 @@ public class CoinSelectionHelpersTests
 		};
 		int limit = 3;
 		long amount = 7;
-		int tolerance = 10;
 
 		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
+		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount);
 
 		// Assert
 		Assert.Equal(new[] { new Money(8) }, GetValues(result));
@@ -114,10 +93,9 @@ public class CoinSelectionHelpersTests
 		};
 		int limit = 3;
 		long amount = 9;
-		int tolerance = 10;
 
 		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
+		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount);
 
 		// Assert
 		Assert.Empty(GetValues(result));
@@ -216,194 +194,6 @@ public class CoinSelectionHelpersTests
 		Assert.Equal(new[] { new Money(5), new Money(5), new Money(10) }, GetValues(result));
 	}
 
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_SmallestFirst_Changeless_InsideToleranceAbove_WhenUTXOsListHasSufficientValuesOnFirstIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(2) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(5) }
-		};
-		int limit = 3;
-		long amount = 7;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(1), new Money(2), new Money(5) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_SmallestFirst_Changeless_InsideToleranceAbove_WhenUTXOsListHasSufficientValuesOnSecondIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(2) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(5) }
-		};
-		int limit = 3;
-		long amount = 10;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(1), new Money(5), new Money(5) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_SmallestFirst_Changeless_InsideToleranceAbove_WhenUTXOsListHasSufficientValuesOnThirdIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(2) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(5) }
-		};
-		int limit = 3;
-		long amount = 14;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(5), new Money(5), new Money(5) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_SmallestFirst_Changeless_InsideToleranceAbove_WhenUTXOsListHasSufficientValuesOnTheNIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(2) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(10) }
-		};
-		int limit = 3;
-		long amount = 19;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(5), new Money(5), new Money(10) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_SmallestFirst_Changeless_InsideToleranceBelow_WhenUTXOsListHasSufficientValuesOnFirstIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(2) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(5) }
-		};
-		int limit = 3;
-		long amount = 9;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(1), new Money(2), new Money(5) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_SmallestFirst_Changeless_InsideToleranceBelow_WhenUTXOsListHasSufficientValuesOnSecondIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(2) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(5) }
-		};
-		int limit = 3;
-		long amount = 12;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(1), new Money(5), new Money(5) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_SmallestFirst_Changeless_InsideToleranceBelow_WhenUTXOsListHasSufficientValuesOnThirdIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(2) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(5) }
-		};
-		int limit = 3;
-		long amount = 16;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(5), new Money(5), new Money(5) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_SmallestFirst_Changeless_InsideToleranceBelow_WhenUTXOsListHasSufficientValuesOnTheNIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(2) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(10) }
-		};
-		int limit = 3;
-		long amount = 21;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(5), new Money(5), new Money(10) }, GetValues(result));
-	}
-
 	/// <summary>
 	/// Tests for SelectCoins when UTXOs are provided in descending order
 	/// </summary>
@@ -426,50 +216,6 @@ public class CoinSelectionHelpersTests
 
 		// Assert
 		Assert.Equal(new[] { new Money(6), new Money(3) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_BiggestFirst_Changeless_InsideToleranceAbove_WhenUTXOsListHasSufficientValues()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(2) },
-			new UTXO { Value = new Money(1) }
-		};
-		int limit = 3;
-		long amount = 7;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(5), new Money(3) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_BiggestFirst_Changeless_InsideToleranceBelow_WhenUTXOsListHasSufficientValues()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(4) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(1) }
-		};
-		int limit = 3;
-		long amount = 9;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(4), new Money(3), new Money(1) }, GetValues(result));
 	}
 
 	/// <summary>
@@ -514,147 +260,5 @@ public class CoinSelectionHelpersTests
 
 		// Assert
 		Assert.Equal(new[] { new Money(5), new Money(6), new Money(4) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_ClosestFirst_Changeless_InsideToleranceAbove_WhenUTXOsListHasSufficientValuesOnSecondIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(4) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(7) },
-			new UTXO { Value = new Money(6) }
-		};
-		int limit = 3;
-		long amount = 11;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(4), new Money(3), new Money(5) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_ClosestFirst_Changeless_InsideToleranceAbove_WhenUTXOsListHasSufficientValuesOnThirdIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(6) },
-			new UTXO { Value = new Money(4) },
-			new UTXO { Value = new Money(7) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(12) },
-		};
-		int limit = 3;
-		long amount = 20;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(6), new Money(12), new Money(3) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_ClosestFirst_Changeless_InsideToleranceAbove_WhenUTXOsListHasSufficientValuesOnTheNIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(6) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(4) },
-			new UTXO { Value = new Money(12) },
-			new UTXO { Value = new Money(15) },
-			new UTXO { Value = new Money(16) }
-		};
-		int limit = 3;
-		long amount = 21;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(6), new Money(12), new Money(4) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_ClosestFirst_Changeless_InsideToleranceBelow_WhenUTXOsListHasSufficientValuesOnSecondIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(4) },
-			new UTXO { Value = new Money(1) },
-			new UTXO { Value = new Money(6) }
-		};
-		int limit = 3;
-		long amount = 9;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(3), new Money(1), new Money(4) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_ClosestFirst_Changeless_InsideToleranceBelow_WhenUTXOsListHasSufficientValuesOnThirdIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(6) },
-			new UTXO { Value = new Money(4) },
-			new UTXO { Value = new Money(7) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(10) },
-		};
-		int limit = 3;
-		long amount = 18;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(6), new Money(4), new Money(7) }, GetValues(result));
-	}
-
-	[Fact]
-	public void SelectCoins_ShouldReturnSelectedCoins_ClosestFirst_Changeless_InsideToleranceBelow_WhenUTXOsListHasSufficientValuesOnTheNIteration()
-	{
-		// Arrange
-		var utxos = new List<UTXO>()
-		{
-			new UTXO { Value = new Money(6) },
-			new UTXO { Value = new Money(5) },
-			new UTXO { Value = new Money(7) },
-			new UTXO { Value = new Money(4) },
-			new UTXO { Value = new Money(8) },
-			new UTXO { Value = new Money(3) },
-			new UTXO { Value = new Money(9) }
-		};
-		int limit = 3;
-		long amount = 19;
-		int tolerance = 10;
-
-		// Act
-		var result = CoinSelectionHelpers.SelectCoins(utxos, limit, amount, tolerance);
-
-		// Assert
-		Assert.Equal(new[] { new Money(6), new Money(5), new Money(7) }, GetValues(result));
 	}
 }
